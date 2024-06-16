3ec899cc68a3f614f338c41565f892244f3d5644
Khair Muhammad <khairmuhammadmemon@healthhub.kr>
2024-06-16 16:08:26 +0500

    initial commit

---
+++ src/main/java/com/khair/zipfilesbackend/ZipFilesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khair.zipfilesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipFilesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipFilesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khair/zipfilesbackend/ZipFilesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khair.zipfilesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipFilesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
